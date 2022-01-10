26ae473bf7a3851aefabfef9fa41bb1e99a63b8d
MarinaBsanz <marinabsanz@gmail.com>
2022-01-10 17:24:09 +0100

    creating new comps manually

---
+++ 01-bases/src/app/contador/contador.component.ts
@@ -0,0 +1,47 @@
+
+import {Component }from '@angular/core';
+
+@Component({ 
+selector: 'app-contador',
+template: `
+
+<h1>{{title}}</h1>
+
+<h2> Acumulo o descuento multiplicando por el número <strong> {{base}} </strong></h2>
+
+<button (click)= acumular(base)>+{{base}}</button>
+
+<button (click)= 'acumular(-base)'>-{{base}}</button>
+
+<span> {{number}} </span>
+
+<!--haciéndolo modo js. Debajo+ limpio añadiendo métodos
+     <button (click)= 'number=number+1;'> +1 </button>
+<button (click)= 'number=number-1'>-1 </button>      -->
+<!--         
+<span> {{number}} </span>
+<button (click)= sume()> +1</button>
+<button (click)= resta()> -1</button>
+ Le añadirçiamos en el archivo ts   en la clase, estas funciones
+sume() {
+  this.number+=1
+}
+ resta() {
+ this.number+=-1
+ }
+ -->
+
+`
+})
+
+export class ContadorComponent {
+    public title:string = 'Contador App';
+    public number: number = 0;
+    public base: number = 2;
+   
+   
+   acumular (valor:number) {
+     this.number +=valor;
+   }
+
+}
