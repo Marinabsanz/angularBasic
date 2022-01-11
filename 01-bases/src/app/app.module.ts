27136e892571d967f8eb37d204fa1c71f632f9f9
MarinaBsanz <marinabsanz@gmail.com>
2022-01-11 17:11:30 +0100

    creatingnewModules

--- 01-bases/src/app/app.module.ts
+++ 01-bases/src/app/app.module.ts
@@ -4,16 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
+import { FuturamaModule } from './futurama/futurama.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+    
    
   ],
   imports: [
     BrowserModule,
     HeroesModule,
-    ContadorModule
+    ContadorModule,
+    FuturamaModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ 01-bases/src/app/futurama/main-page/main-page.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+})
+export class MainPageComponent {
+  public title:string = 'personajes'
+
+
+
+  constructor() { 
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
+
