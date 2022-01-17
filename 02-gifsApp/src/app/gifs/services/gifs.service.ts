65cb16f67507aa40afc7d72a290041ae7757918f
MarinaBsanz <marinabsanz@gmail.com>
2022-01-17 13:40:53 +0100

    using HTTP instead fetch-observables

--- 02-gifsApp/src/app/gifs/services/gifs.service.ts
+++ 02-gifsApp/src/app/gifs/services/gifs.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
+
+
+private apiKey: string = '6RSpIq9Qp5iiCEUwO5v0jYmbOA78ftZn'
 
 private _historic: string [] = [];
 
@@ -12,6 +16,10 @@ get historic() {
 
 return [...this._historic]
 }
+
+constructor(private http: HttpClient){
+}
+
 searchGifs(query: string= ''){
 
 query = query.trim().toLowerCase();
@@ -19,12 +27,28 @@ query = query.trim().toLowerCase();
  //evitar repeticiones en la búsqueda
   if (!this._historic.includes(query)){
     this._historic.unshift(query);
-  }  
   this._historic= this._historic.splice(0,10);
-
-  console.log(this._historic)
 }
 
+this.http.get('https://api.giphy.com/v1/gifs/search?api_key=6RSpIq9Qp5iiCEUwO5v0jYmbOA78ftZn&q=harry potter&limit=10' )
+.subscribe( (response:any) => {
+
+console.log( response.data )
+
+});
+
+
 }
 
 
+}
+
+
+
+
+
+//MODO JS puro con fetch
+//fetch ('https://api.giphy.com/v1/gifs/search?api_key=6RSpIq9Qp5iiCEUwO5v0jYmbOA78ftZn&q=harry potter')
+//.then (resp => {
+//resp.json().then(data=>{
+// console.log(data); }) })
